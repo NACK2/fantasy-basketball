dd0240f573bd6ae8ff618eb1ae78290b97db2bfd
Nick <nickkang@student.ubc.ca>
2022-11-18 13:51:03 -0800

    Added play game button and text field

--- src/main/ui/Gui.java
+++ src/main/ui/Gui.java
@@ -3,42 +3,105 @@ package ui;
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 // https://www.youtube.com/watch?v=Kmgo00avvEw
+// 25 mins
 // GUI made with swing
-public class Gui extends JFrame {
-    private JLabel label;
+public class Gui extends JFrame implements ActionListener {
+    private JLabel startUpLabel;
+    private JLabel playGameLabel;
 
-    // EFFECTS:
+    private JTextField textUserName;
+
+    private JButton playBtn;
+    private ImageIcon basketballCourtImg;
+    private Border border;
+
+    // EFFECTS: Runs the GUI made with Swing
     public Gui() {
-        this.label = new JLabel();
+        setVisible(true);
+        setResizable(false);
+        setSize(900, 700);
+        //setLayout(new FlowLayout());
         getContentPane().setBackground(Color.BLACK); // setting background colour to black
-        startUpScreen();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        startUpScreen(); // Start up screen
+        startUpScreenButtons(); // Buttons displayed on start up screen
     }
 
+    // MODIFIES: this
     // EFFECTS: Displays start up screen
     public void startUpScreen() {
-        add(label); // adding label to JFrame
+        startUpLabel = new JLabel();
+        add(startUpLabel); // adding label to JFrame
         setTitle("Fantasy Basketball App by Nicholas Kang");
-        setSize(900, 700);
-        setVisible(true);
 
-        // exits application instead of hiding it when hitting exit button in top right\
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setResizable(false);
+        basketballCourtImg = new ImageIcon("./data/EmptyBasketballCourt.jpg");
+        border = BorderFactory.createLineBorder(new Color(51, 51, 255), 3);
+        startUpLabel.setBorder(border);
 
-        ImageIcon img = new ImageIcon("./data/EmptyBasketballCourt.jpg");
-        Border border = BorderFactory.createLineBorder(new Color(51, 51, 255), 2);
+        startUpLabel.setIcon(basketballCourtImg); // using image as background
+        startUpLabel.setText("Welcome to Fantasy Basketball!"); // Label displays this text
+        startUpLabel.setFont(new Font(Font.MONOSPACED, Font.BOLD, 25));
+        startUpLabel.setForeground(Color.white);
 
-        label.setIcon(img); // using image as background
-        label.setText("Welcome to Fantasy Basketball!"); // Label displays this text
-        label.setFont(new Font(Font.MONOSPACED, Font.BOLD, 20));
-        label.setForeground(Color.white);
-        label.setBorder(border);
+        startUpLabel.setHorizontalTextPosition(JLabel.CENTER);
+        startUpLabel.setVerticalTextPosition(JLabel.TOP);
+        startUpLabel.setHorizontalAlignment(JLabel.CENTER);
+        startUpLabel.setVerticalAlignment(JLabel.CENTER);
+    }
 
-        label.setHorizontalTextPosition(label.CENTER);
-        label.setVerticalTextPosition(label.TOP);
-        label.setHorizontalAlignment(label.CENTER);
-        label.setVerticalAlignment(label.CENTER);
+    // EFFECTS: Displays buttons on start up screen
+    public void startUpScreenButtons() {
+        playButton();
+    }
+
+    // MODIFIES: this
+    // EFFECTS: Displays play button
+    public void playButton() {
+        playBtn = new JButton("Play");
+        playBtn.setBounds(390, 550, 120, 50);
+        playBtn.setBorder(BorderFactory.createEtchedBorder());
+        playBtn.setBackground(Color.LIGHT_GRAY);
+        playBtn.setForeground(Color.BLACK);
+        playBtn.setFont(new Font("Plain", Font.BOLD, 20));
+        startUpLabel.add(playBtn);
+        add(startUpLabel);
+        playBtn.addActionListener(this);
+        playBtn.setFocusable(false);
+    }
+
+    public void playGame() {
+        playGameLabel = new JLabel();
+        add(playGameLabel);
+
+        border = BorderFactory.createLineBorder(new Color(51, 51, 255), 3);
+        playGameLabel.setBorder(border);
+        playGameLabel.setIcon(basketballCourtImg);
+
+        playGameLabel.setHorizontalAlignment(JLabel.CENTER);
+        playGameLabel.setVerticalAlignment(JLabel.CENTER);
+    }
+
+    public void getUsers() {
+        textUserName = new JTextField();
+        textUserName.setPreferredSize(new Dimension(25, 40));
+        playGameLabel.add(textUserName);
+        add(playGameLabel);
+        textUserName.setBounds(390, 550, 120, 50);
+
+    }
+
+    // Listens for events
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == playBtn) {
+            startUpLabel.setVisible(false);
+            playGame();
+            getUsers();
+        }
     }
 }
